ee555b7b63ad4892381b0797023d47ad07ed85f3
chung-coder <peggy881203@gmail.com>
2020-12-11 01:42:38 +0800

    keyboard & callback_query

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,52 +1,120 @@
 import logging
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    MessageHandler,
+    CallbackQueryHandler,
+    Filters,
+    ConversationHandler,
+    CallbackContext,
+)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
+# state
+AGE = range(1)
+
 
 def start(update: Update, context: CallbackContext) -> None:
+
     keyboard = [
         [
-            InlineKeyboardButton("成熟的大人 - 米咖儂", callback_data='成熟的大人 - 米咖儂'),
+            InlineKeyboardButton(
+                text="是，我滿18歲了!", callback_data="Yes"),
+            InlineKeyboardButton(text="還沒，我未滿18歲", callback_data="No"),
         ],
-        [
-            InlineKeyboardButton("米蔓天使 - 米蔓", callback_data='米蔓天使 - 米蔓'),
-        ],
-        [
-            InlineKeyboardButton("少女心 - 米美雪", callback_data='少女心 - 米美雪'),
-        ]
     ]
 
-    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text(
+        "嗨! 我是Tender Botter，很高興能為您服務!"
+    )
 
-    update.message.reply_text('請選擇你的調酒:', reply_markup=reply_markup)
+    update.message.reply_text(
+        "請問您是否已滿18歲?",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+    return AGE
 
 
-def button(update: Update, context: CallbackContext) -> None:
+def drinkWine(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
 
-    query.answer()
+    # 使用者已成年
+    if query.data == "Yes":
 
-    query.edit_message_text(text=f"你選擇的調酒是 {query.data}")
+        keyboard = [
+            [
+                InlineKeyboardButton(
+                    "成熟的大人 - 米咖儂", callback_data='成熟的大人 - 米咖儂'),
+            ],
+            [
+                InlineKeyboardButton("米蔓天使 - 米蔓", callback_data='米蔓天使 - 米蔓'),
+            ],
+            [
+                InlineKeyboardButton("少女心 - 米美雪", callback_data='少女心 - 米美雪'),
+            ]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
 
+        query.edit_message_text(
+            text="請選擇您的調酒品項", reply_markup=reply_markup
+        )
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("輸入 /start 開始調酒")
+    # 使用者未成年
+    elif query.data == "No":
+        keyboard = [
+            [
+                InlineKeyboardButton(
+                    "來點氣泡吧 - 雪碧", callback_data='來點氣泡吧 - 雪碧'),
+            ],
+            [
+                InlineKeyboardButton(
+                    "成熟的味道 - 伯朗咖啡", callback_data='成熟的味道 - 伯朗咖啡'),
+            ],
+            [
+                InlineKeyboardButton("新鮮純淨 - 牛乳", callback_data='新鮮純淨 - 牛乳'),
+            ],
+            [
+                InlineKeyboardButton("酸甜滋味 - 蔓越莓", callback_data='酸甜滋味 - 蔓越莓'),
+            ],
+            [
+                InlineKeyboardButton("清涼果汁 - 美粒果", callback_data='清涼果汁 - 美粒果'),
+            ],
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        query.edit_message_text(
+            text="未滿18歲禁止飲酒，請選擇您的飲品", reply_markup=reply_markup
+        )
+
+    return AGE
 
 
 def main():
 
     updater = Updater(
-        "my-token", use_context=True)
+        "1202208172:AAFdfP6gj-fgaRVO3t6nhvek7B7mXQPOMZQ", use_context=True)
 
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            AGE: [
+                CallbackQueryHandler(drinkWine),
+            ],
+        },
+        fallbacks=[CommandHandler('start', start)],
+    )
+
+    dispatcher.add_handler(conv_handler)
 
     # Start the Bot
     updater.start_polling()
